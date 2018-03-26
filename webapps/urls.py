4c01e7a20d22394f41c09f2f35932f8685916238
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-03-26 14:21:31 -0400

    Create LoginView.

--- webapps/urls.py
+++ webapps/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from socialnetwork import views
 
 urlpatterns = [
     url(r'^monopoly/', include('monopoly.urls')),
