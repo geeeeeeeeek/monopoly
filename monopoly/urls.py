cfa12c6bf09793aa3665a1a4b2bb354c82b3e916
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-03-27 17:50:39 -0400

    Finish profile page backbone.

--- monopoly/urls.py
+++ monopoly/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from monopoly.views.login_view import LoginView
+from monopoly.views.profile_view import ProfileView
 
 urlpatterns = [
     url(r'^login', LoginView.as_view(), name='login'),
+    url(r'^profile', ProfileView.as_view(), name='profile')
 ]
