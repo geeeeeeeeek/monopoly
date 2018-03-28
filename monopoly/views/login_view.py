5d27222a074f7ee7cb3c1246090a3c352c8b51d2
weizzzzzz <weizil@andrew.cmu.edu>
2018-03-27 21:00:34 -0400

    Add register page

--- monopoly/views/login_view.py
+++ monopoly/views/login_view.py
@@ -3,8 +3,12 @@ from django.views import View
 
 
 class LoginView(View):
-    initial = {'key': 'value'}
+    initial = {'active_page': 'register'}
     template_name = 'login_view.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {})
+        return render(request, self.template_name, {
+            "active_page": "login",
+            "error": None
+        })
+
