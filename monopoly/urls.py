53a15ab9188a65ab5dab0728a39504e9dac6e00b
weizzzzzz <weizil@andrew.cmu.edu>
2018-03-28 22:24:38 -0400

    Add login required

--- monopoly/urls.py
+++ monopoly/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from monopoly.views.login_view import LoginView
 from monopoly.views.register_view import RegisterView
@@ -9,13 +10,12 @@ from monopoly.views.register_confirm_view import ConfirmRegistrationView
 from monopoly.views.photo_view import PhotoView
 
 urlpatterns = [
-    url(r'^$', GameView.as_view(), name='game'),
+    url(r'^$', login_required(GameView.as_view()), name='game'),
     url(r'^login', LoginView.as_view(), name='login'),
-    url(r'^profile/(?P<profile_user>[a-zA-Z0-9]+)$', ProfileView.as_view(), name='profile'),
-    url(r'^join', JoinView.as_view(), name="join"),
+    url(r'^profile/(?P<profile_user>[a-zA-Z0-9]+)$', login_required(ProfileView.as_view()), name='profile'),
+    url(r'^join', login_required(JoinView.as_view()), name="join"),
     url(r'^register', RegisterView.as_view(), name='register'),
-    url(r'^photo/(?P<username>[a-zA-Z0-9]+)$', PhotoView.as_view(), name='photo'),
+    url(r'^photo/(?P<username>[a-zA-Z0-9]+)$', login_required(PhotoView.as_view()), name='photo'),
     url(r'^confirm-registration/(?P<username>[a-zA-Z0-9]+)/(?P<token>[a-z0-9\-]+)$',
         ConfirmRegistrationView.as_view(), name='confirm'),
-
 ]
