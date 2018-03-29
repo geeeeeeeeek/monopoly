56a6824af03f5134f419a6f54a9bba4c0f0996fc
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-03-28 21:41:06 -0400

    Finish JoinView backend.

--- monopoly/urls.py
+++ monopoly/urls.py
@@ -1,21 +1,20 @@
 from django.conf.urls import url
 
-from monopoly.views.login_view import LoginView
-from monopoly.views.register_view import RegisterView
-from monopoly.views.profile_view import ProfileView
-from monopoly.views.join_view import JoinView
 from monopoly.views.game_view import GameView
-from monopoly.views.register_confirm_view import ConfirmRegistrationView
+from monopoly.views.join_view import JoinView
+from monopoly.views.login_view import LoginView
 from monopoly.views.photo_view import PhotoView
+from monopoly.views.profile_view import ProfileView
+from monopoly.views.register_confirm_view import ConfirmRegistrationView
+from monopoly.views.register_view import RegisterView
 
 urlpatterns = [
     url(r'^$', GameView.as_view(), name='game'),
     url(r'^login', LoginView.as_view(), name='login'),
     url(r'^profile/(?P<profile_user>[a-zA-Z0-9]+)$', ProfileView.as_view(), name='profile'),
-    url(r'^join', JoinView.as_view(), name="join"),
+    url(r'^join/(?P<host_name>.+)', JoinView.as_view(), name="join"),
     url(r'^register', RegisterView.as_view(), name='register'),
     url(r'^photo/(?P<username>[a-zA-Z0-9]+)$', PhotoView.as_view(), name='photo'),
     url(r'^confirm-registration/(?P<username>[a-zA-Z0-9]+)/(?P<token>[a-z0-9\-]+)$',
-        ConfirmRegistrationView.as_view(), name='confirm'),
-
+        ConfirmRegistrationView.as_view(), name='confirm')
 ]
