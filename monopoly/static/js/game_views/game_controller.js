038bdc32205136bf427a2cde966ee9b34a3f929a
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-04-08 21:15:45 -0400

    Add rolling dice animation.

--- monopoly/static/js/game_views/game_controller.js
+++ monopoly/static/js/game_views/game_controller.js
@@ -15,14 +15,14 @@ class GameController {
         });
 
         this.boardController.drawBoard(() => {
-            this.boardController.drawPlayers(4);
+            // this.boardController.drawPlayers(4);
             // setTimeout(() => {
             //     this.movePlayer(0, 3);
             //     this.movePlayer(1, 9);
             //     this.movePlayer(2, 14);
             //     this.movePlayer(3, 28);
-            // }, 10000);
-            this.addProperty(PropertyManager.PROPERTY_HOUSE, 3);
+            // }, 15000);
+            // this.addProperty(PropertyManager.PROPERTY_HOUSE, 3);
             // this.addProperty(PropertyManager.PROPERTY_HOUSE, 3);
             // this.addProperty(PropertyManager.PROPERTY_HOUSE, 3);
             //
@@ -33,7 +33,7 @@ class GameController {
             // this.addProperty(PropertyManager.PROPERTY_HOUSE, 13);
             //
             // this.addProperty(PropertyManager.PROPERTY_HOTEL, 6);
-            this.addProperty(PropertyManager.PROPERTY_HOTEL, 8);
+            // this.addProperty(PropertyManager.PROPERTY_HOTEL, 8);
             // this.addProperty(PropertyManager.PROPERTY_HOTEL, 37);
             // this.addProperty(PropertyManager.PROPERTY_HOTEL, 31);
         });
