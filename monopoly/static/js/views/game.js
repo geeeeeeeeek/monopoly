3c8e204ed86b79e1a24d9b3ecbe0472d8564b601
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-04-09 02:27:06 -0400

    Support init users.

--- monopoly/static/js/views/game.js
+++ monopoly/static/js/views/game.js
@@ -10,17 +10,19 @@ class GameView {
         this.isChatShown = false;
         this.$chatSwitch = document.getElementById("chat-switch");
         this.$chatCard = document.getElementById("chat-card");
+        this.$usersContainer = document.getElementById("users-container");
 
-        this.$chatSwitch.addEventListener("click", () => {
-            this.isChatShown = !this.isChatShown;
-            if (this.isChatShown) {
-                this.$chatCard.classList.remove("hidden");
-            } else {
-                this.$chatCard.classList.add("hidden");
-            }
-        });
+        // this.$chatSwitch.addEventListener("click", () => {
+        //     this.isChatShown = !this.isChatShown;
+        //     if (this.isChatShown) {
+        //         this.$chatCard.classList.remove("hidden");
+        //     } else {
+        //         this.$chatCard.classList.add("hidden");
+        //     }
+        // });
 
-        this.initGameBoard();
+        // this.initGameBoard();
+        this.initGame();
     }
 
     initGameBoard() {
@@ -31,6 +33,30 @@ class GameView {
             // The base URL from where the BoardController will load its data.
             assetsUrl: '/static/3d_assets/'
         });
+    }
+
+    initGame() {
+        this.initPlayers([
+            {
+                avatar: "/static/images/favicon.png"
+            }, {
+                avatar: "/static/images/favicon.png"
+            }, {
+                avatar: "/static/images/favicon.png"
+            }, {
+                avatar: "/static/images/favicon.png"
+            }
+        ])
+    }
+
+    initPlayers(users) {
+        for (let i = 0; i < users.length; i++) {
+            this.$usersContainer.innerHTML += `
+                <div class="user-group">
+                    <img class="user-avatar" src="${users[i].avatar}">
+                    <img class="user-role" src="/static/images/player_${i}.png">
+                </div>`;
+        }
     }
 }
 
