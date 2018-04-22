853372632c8713f350a55c6983d17d2daffcbd1b
Liangchen Yi <liangchy@andrew.cmu.edu>
2018-04-22 19:34:44 -0400

    add a function to get all lands's owners index

--- monopoly/core/test_core.py
+++ monopoly/core/test_core.py
@@ -520,6 +520,34 @@ def test17(number):
     print 'successful test{0}\n'.format(number)
 
 
+# get all the land owners
+def test18(number):
+    print "---test{0}----".format(number)
+    game = Game(2)
+    game.get_current_player().set_money(80)
+    money = game.get_current_player().get_money()
+    print money
+    handler = LogHandler(game)
+    game.add_game_change_listner(handler)
+
+    steps, move_result = game.roll(3)
+    # result
+    if move_result.move_result_type == MoveResultType.CONSTRUCTION_OPTION \
+            or move_result.move_result_type == MoveResultType.BUY_LAND_OPTION:
+        move_result.set_decision(True)
+    print 'move result type is', move_result.get_move_result_type()
+    print 'whether it is an option: ', move_result.is_option()
+    print "steps: ", steps
+    print move_result
+    game.make_decision(move_result)
+    print game.get_status()
+
+    print game.get_land_owners()
+    assert len(game.get_land_owners()) == 40
+
+    print 'successful test{0}\n'.format(number)
+
+
 class LogHandler(MonopolyHandler):
 
     def __init__(self, g):
@@ -572,6 +600,7 @@ def test_suite():
     test15(15)
     test16(16)
     test17(17)
+    test18(18)
 
 
 if __name__ == "__main__":
