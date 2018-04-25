2a701ff212bbaa5af411edff311ef420188d1767
weizzzzzz <weizil@andrew.cmu.edu>
2018-04-25 10:44:14 -0400

    turn debug off

--- webapps/settings.py
+++ webapps/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7d#m%i!ed32ei5v(i2da*#w9od=_m9u&*6&-6jla1fqdb=66_('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
